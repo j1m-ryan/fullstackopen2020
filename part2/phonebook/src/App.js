60b3abf0cd5d3e914f99f7b8aeed17183cd89b53
Jim Ryan <jimmih15@gmail.com>
2020-03-17 09:42:54 +0000

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,58 @@
 import React, { useState } from "react";
+const Numbers = props => {
+  const persons = props.persons;
+  const filterWord = props.filterWord;
+  if (filterWord === "") {
+    return (
+      <>
+        <h2>Numbers</h2>
+        {persons.map((person, i) => (
+          <span key={i}>
+            {person.name} {person.number}
+            <br />
+          </span>
+        ))}
+      </>
+    );
+  } else {
+    let newObj = [];
+    console.log("newObj is a", typeof newObj);
+    for (let i = 0; i < persons.length; i++) {
+      if (persons[i].name.includes(filterWord)) {
+        console.log("person name", persons[i].name, "filterword", filterWord);
+        newObj = newObj.concat({
+          name: persons[i].name,
+          number: persons[i].number
+        });
+      }
+    }
+    console.log(newObj);
+
+    return (
+      <>
+        <h2>Numbers</h2>
+        {newObj.map((person, i) => (
+          <span key={i}>
+            {person.name} {person.number}
+            <br />
+          </span>
+        ))}
+      </>
+    );
+  }
+};
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterWord, setFilter] = useState("");
 
   const handleNameChange = event => {
     setNewName(event.target.value);
+  };
+  const handleFilter = event => {
+    setFilter(event.target.value);
   };
   const handleNumberChange = event => {
     setNewNumber(event.target.value);
@@ -24,6 +70,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input value={filterWord} onChange={handleFilter} />
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -36,13 +84,7 @@ const App = () => {
           </button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map((person, i) => (
-        <span key={i}>
-          {person.name}
-          <br />
-        </span>
-      ))}
+      <Numbers persons={persons} filterWord={filterWord} />
     </div>
   );
 };
