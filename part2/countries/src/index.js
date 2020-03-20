2dc7b6c2a4b2bb3347fbda4f30f372d3c6b4a934
Jim Ryan <jimmih15@gmail.com>
2020-03-20 13:44:35 +0000

    2.14*: Data for countries, step3
    Changed api_key to an environment variable

--- part2/countries/src/index.js
+++ part2/countries/src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-//let api_key = process.env.REACT_APP_API_KEY;
-const api_key = "63aa57dc24fd32045c59858b3cb51584";
+
+const api_key = process.env.REACT_APP_API_KEY;
 ReactDOM.render(<App api_key={api_key} />, document.getElementById("root"));
