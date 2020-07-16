1c7e08d4cc2f99a9099ed1566196c75fff03d9f5
J1M-RYAN <jimmih15@gmail.com>
2020-07-16 20:23:47 +0100

    4.5*: helper functions and unit tests, step3

--- part4/tests/blog_methods.test.js
+++ part4/tests/blog_methods.test.js
@@ -1,4 +1,4 @@
-const { totalLikes } = require("../utils/list_helper");
+const { totalLikes, favouriteBlog } = require("../utils/list_helper");
 describe("total likes", () => {
   test("works on list with one post", () => {
     const post = [
@@ -48,5 +48,65 @@ describe("total likes", () => {
 
   test("works where there are no posts", () => {
     expect(totalLikes([])).toBe(0);
+  });
+});
+
+describe("favourite blog works on", () => {
+  test("one blog post", () => {
+    const post = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url:
+          "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+    ];
+    expect(favouriteBlog(post)).toEqual({
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 5,
+    });
+  });
+
+  test("many blog posts", () => {
+    const posts = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url:
+          "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "A blog title",
+        author: "Another person",
+        url: "http://www.google.com",
+        likes: 2,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Some title",
+        author: "Someone else",
+        url: "http://www.jim.com",
+        likes: 1,
+        __v: 0,
+      },
+    ];
+    expect(favouriteBlog(posts)).toEqual({
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 5,
+    });
+  });
+  test("no blog posts", () => {
+    const post = [];
+    expect(favouriteBlog(post)).toEqual({});
   });
 });
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -10,4 +12,21 @@ const totalLikes = (blogPosts) => {
   return total;
 };
 
-module.exports = { dummy, totalLikes };
+const favouriteBlog = (blogPosts) => {
+  if (blogPosts === null || blogPosts.length === 0) return {};
+  let maxLikes = 0;
+  let favouritePost;
+  blogPosts.forEach((e) => {
+    if (e.likes > maxLikes) {
+      favouritePost = e;
+      maxLikes = e.likes;
+    }
+  });
+  return {
+    title: favouritePost.title,
+    author: favouritePost.author,
+    likes: favouritePost.likes,
+  };
+};
+
+module.exports = { dummy, totalLikes, favouriteBlog };
